c13a3d08f8f3a6b5717df234343442a3ddc37f29
Stefan <sysdist.com@gmail.com>
2018-10-10 16:06:02 +0200

    sidebar with extra links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,3 +25,24 @@ $(function () {
 	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 	});
 });
+$(function () {
+    $('.navbar-toggle2').click(function () {
+        $('.navbar-nav2').toggleClass('slide-in');
+        $('.side-body').toggleClass('body-slide-in');
+        $('#search').removeClass('in').addClass('collapse').slideUp(200);
+
+        /// uncomment code for absolute positioning tweek see top comment in css
+        //$('.absolute-wrapper').toggleClass('slide-in');
+        
+    });
+   
+   // Remove menu for searching
+   $('#search-trigger').click(function () {
+        $('.navbar-nav').removeClass('slide-in');
+        $('.side-body').removeClass('body-slide-in');
+
+        /// uncomment code for absolute positioning tweek see top comment in css
+        //$('.absolute-wrapper').removeClass('slide-in');
+
+    });
+});
